d6325f6a18ad6cde5feb94cf5ed7322c9e7876e1
Zeyu Yang <zeyuyang8@gmail.com>
2024-09-03 12:12:07 -0500

    .

--- run_clsp_pm_2.sh
+++ run_clsp_pm_2.sh
@@ -4,7 +4,8 @@ for fold in 0 1 2 3 4; do
             for seq_enc in transformer; do
                 for tab_enc in resnet; do
                     for exclude in label all; do
-                        for label_ratio in 0.1 0.5 1.0; do
+                        # for label_ratio in 0.1 0.5 1.0; do
+                        for label_ratio in 0.01; do
                             for unlabel_ratio in 1.0; do
                                 python main.py --train --dataset pmdata --exp run --mode clip --linear --finetune --n_epochs 300 --batch_size 512 --seq_enc $seq_enc --tab_enc $tab_enc --fold $fold --norm_config $norm_config --seed $seed --exclude $exclude --unlabel_ratio $unlabel_ratio --label_ratio $label_ratio
                             done
